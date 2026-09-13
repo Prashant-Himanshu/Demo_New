ea3d1364976f4b7e37015391538a5d6467d9e47e
Prashant Singh <abc.gmail.com>
2026-09-13 21:52:05 +0530

    Thirst Commit

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("This is first version before pushing on git");
+            Console.WriteLine("First Change in Main Method"); 
+
+
+
         }
     }
 }
