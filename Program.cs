224c3bec99857848e1c133bee718e0ee4b752636
Prashant Singh <abc.gmail.com>
2026-09-13 22:09:18 +0530

    Forthe Commit

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-﻿namespace Demo_New
+﻿using Application1;
+using Application2;
+
+namespace Demo_New
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("This is first version before pushing on git");
-            Console.WriteLine("First Change in Main Method"); 
+            Console.WriteLine("First Change in Main Method");
+
+            Application1.Class1 c = new Application1.Class1();
+            c.Display();
 
 
 
