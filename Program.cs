42f701b7262aa2f049fc9ada3e9bb716deb8a311
Prashant Singh <abc.gmail.com>
2026-09-13 21:44:41 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is first version before pushing on git");
+        }
+    }
+}
